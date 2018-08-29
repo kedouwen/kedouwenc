e2892a07db8afcdda86e25888aabce337aae18cf
kedouwen <li.jun0717@aliyun.com>
2018-08-29 20:13:05 +0800

    jsontoarray

--- kedouwenc/jsontoarray.Designer.cs
+++ kedouwenc/jsontoarray.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -50,10 +51,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 566);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "清空！";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // jsontoarray
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "jsontoarray";
@@ -67,5 +79,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- kedouwenc/jsontoarray.cs
+++ kedouwenc/jsontoarray.cs
@@ -23,7 +23,7 @@ namespace kedouwenc
             string[] strarr = str.Split(Environment.NewLine.ToCharArray());
             //string resultarr = string.Empty; 
             int k = 0;
-            string[,] resultarr = new string[strarr.Length, 2];
+            string[,] resultarr = new string[strarr.Length, 8];
 
             for (int i = 0; i < strarr.Length; i++)
             {
@@ -38,12 +38,20 @@ namespace kedouwenc
 
                     resultarr[k, 0] = strarr[i].Split(':')[0].Trim().Replace("\"", "");
                     resultarr[k, 1] = strarr[i].Split('/')[2].Trim();
+                    resultarr[k, 2] = "VARCHAR2";
+                    resultarr[k, 7] = strarr[i].Split(':')[0].Trim().Replace("\"", "");
                     k = k + 1;
 
                 }
             }
             Excel.Range rng = (Excel.Range)Globals.ThisAddIn.Application.InputBox(Prompt: "请选择存放区域，选择单个单元格即可", Type: 8);
-            rng.Offset[0, 0].Resize[resultarr.GetUpperBound(0) + 1, 2].Value = resultarr;
+            rng.Offset[0, 0].Resize[resultarr.GetUpperBound(0) + 1, 8].Value = resultarr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+
         }
     }
 }
